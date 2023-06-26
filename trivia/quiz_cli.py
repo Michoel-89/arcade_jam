2825809e7ec9fc941e4b6a0b3e61a5d9a282b3f7
Angela Shen <angelayshen@gmail.com>
2023-06-26 16:41:41 -0400

    add trivia game v1.0

---
+++ trivia/quiz_cli.py
@@ -0,0 +1,111 @@
+import sqlite3
+import random
+from quiz_questions import questions
+
+# Connect to the SQLite database
+conn = sqlite3.connect('quiz_scores.db')
+c = conn.cursor()
+
+# Create a table to store quiz scores if it doesn't exist
+c.execute('''CREATE TABLE IF NOT EXISTS scores
+             (name text, score int)''')
+
+def print_highest_score(player_name):
+    # Query the database for the highest score of the specified player
+    c.execute("SELECT MAX(score) FROM scores WHERE name=?", (player_name,))
+    highest_score = c.fetchone()[0]
+
+    if highest_score is not None:
+        print(f"Highest score for {player_name}: {highest_score}")
+    else:
+        print("No past scores found for the player.")
+
+# Prompt the player to enter their name
+player_name = input("Enter your name: ")
+
+# Call the function to print the highest score for the player
+print_highest_score(player_name)
+
+print()  # Print a blank line for separation
+
+# Prompt the player to select a category
+category_num = 1
+print('Choose from the following categories:')
+for category in questions['trivia categories']:
+    print(f"{category_num}. {questions['trivia categories'][category_num - 1]}")
+    category_num += 1
+category_selection = input("Select a category (1-4): ")
+
+# Show questions from the selected category
+while category_selection not in [1, 2, 3, 4]:
+    if category_selection.isdigit() and 1 <= int(category_selection) <= 4:
+        category_selection = int(category_selection)
+        selected_category = questions['trivia categories'][category_selection-1]
+    else:
+        print("Invalid input. Enter a number 1-4.")
+        category_selection = input("Select a category (1-4): ")
+
+print()  # Print a blank line for separation
+
+# Shuffle the questions
+random.shuffle(questions[selected_category])
+
+# Initialize variables to track score and question number
+score = 0
+question_num = 1
+
+# Iterate through each question, limited to 5 questions
+total_num_questions = 5
+for question in questions[selected_category][:total_num_questions]:
+    print(f"Question {question_num}: {question['question']}")
+
+    # Shuffle the answer options
+    random.shuffle(question['options'])
+    
+    # Print the answer options
+    for i, option in enumerate(question['options']):
+        print(f"{i+1}. {option}")
+    
+    # Get the player's answer
+    player_answer = input("Enter your answer (1-4): ")
+    
+    # Validate the answer
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    NC='\033[0m' # No Color
+
+    while player_answer not in [1, 2, 3, 4]:
+        if player_answer.isdigit() and 1 <= int(player_answer) <= 4:
+            player_answer = int(player_answer)
+            if question['options'][player_answer-1] == question['answer']:
+                print(f"{GREEN}Correct!{NC}")
+                score += 1
+            else:
+                print(f"{RED}Wrong!{NC} The correct answer is: {question['answer']}")
+        else:
+            print("Invalid input. Enter a number 1-4.")
+            player_answer = input("Enter your answer (1-4): ")
+            
+    print()  # Print a blank line for separation
+    
+    # Increment the question number
+    question_num += 1
+
+# Store the user's score in the database
+c.execute("INSERT INTO scores VALUES (?, ?)", (player_name, score))
+conn.commit()
+
+# Print the final score
+print(f"Quiz complete! Your score is: {score}/{total_num_questions}")
+
+# def delete_all_scores():
+#     # Execute an SQL statement to delete all records from the "scores" table
+#     c.execute("DELETE FROM scores")
+#     conn.commit()
+#     print("All scores have been deleted.")
+
+# # Call the function to delete all scores
+# delete_all_scores()
+
+# Close the database connection
+conn.close()
---
+++ trivia/quiz_questions.py
@@ -0,0 +1,212 @@
+# Define a list of questions and their corresponding answers
+questions = {
+    "trivia categories": ['Historical Figures', 'Music Artists', 'TV Shows', 'Animal Planet'],
+    "Historical Figures": [
+        {
+            "question": "Who was the leader of the Soviet Union during World War II?",
+            "options": ["Joseph Stalin", "Vladimir Lenin", "Mikhail Gorbachev", "Nikita Khrushchev"],
+            "answer": "Joseph Stalin"
+        },
+        {
+            "question": "Which historical figure is known for writing the 'I Have a Dream' speech?",
+            "options": ["Martin Luther King Jr.", "Abraham Lincoln", "Nelson Mandela", "Winston Churchill"],
+            "answer": "Martin Luther King Jr."
+        },
+        {
+            "question": "Who is known for inventing the telephone?",
+            "options": ["Alexander Graham Bell", "Thomas Edison", "Nikola Tesla", "Guglielmo Marconi"],
+            "answer": "Alexander Graham Bell"
+        },
+        {
+            "question": "Who was the first woman to win a Nobel Prize?",
+            "options": ["Marie Curie", "Rosalind Franklin", "Ada Lovelace", "Jane Goodall"],
+            "answer": "Marie Curie"
+        },
+        {
+            "question": "Who is regarded as the 'Father of the Computer'?",
+            "options": ["Alan Turing", "Steve Jobs", "Bill Gates", "Charles Babbage"],
+            "answer": "Charles Babbage"
+        },
+        {
+            "question": "Who is known for writing 'To Kill a Mockingbird'?",
+            "options": ["Harper Lee", "Ernest Hemingway", "F. Scott Fitzgerald", "Mark Twain"],
+            "answer": "Harper Lee"
+        },
+        {
+            "question": "Which historical figure is known for the theory of relativity?",
+            "options": ["Albert Einstein", "Isaac Newton", "Galileo Galilei", "Nikola Tesla"],
+            "answer": "Albert Einstein"
+        },
+        {
+            "question": "Which historical figure is known for painting 'Starry Night'?",
+            "options": ["Vincent van Gogh", "Leonardo da Vinci", "Pablo Picasso", "Claude Monet"],
+            "answer": "Vincent van Gogh"
+        },
+        {
+            "question": "Who was the first female Prime Minister of the United Kingdom?",
+            "options": ["Margaret Thatcher", "Angela Merkel", "Indira Gandhi", "Theresa May"],
+            "answer": "Margaret Thatcher"
+        },
+        {
+            "question": "Which historical figure is known for his theory of evolution by natural selection?",
+            "options": ["Charles Darwin", "Sigmund Freud", "Max Planck", "Gregor Mendel"],
+            "answer": "Charles Darwin"
+        },
+        {
+            "question": "Which historical figure is known for his contributions to the theory of gravity?",
+            "options": ["Isaac Newton", "Albert Einstein", "Galileo Galilei", "Nikola Tesla"],
+            "answer": "Isaac Newton"
+        }
+    ],
+    "Music Artists": [
+        {
+            "question": "Which artist released the album 'Thriller'?",
+            "options": ["Michael Jackson", "Madonna", "Prince", "Elvis Presley"],
+            "answer": "Michael Jackson"
+        },
+        {
+            "question": "Which artist has the most Grammy?",
+            "options": ["Madonna", "Beyoncé", "Taylor Swift", "Rihanna"],
+            "answer": "Beyoncé"
+        },
+        {
+            "question": "Which artist is known for the song 'Bohemian Rhapsody'?",
+            "options": ["Queen", "The Beatles", "Elton John", "Led Zeppelin"],
+            "answer": "Queen"
+        },
+        {
+            "question": "Who is the lead vocalist of the band U2?",
+            "options": ["Bono", "Chris Martin", "Freddie Mercury", "Bruce Springsteen"],
+            "answer": "Bono"
+        },
+        {
+            "question": "Which artist released the album 'Purple Rain'?",
+            "options": ["Prince", "David Bowie", "Stevie Wonder", "Bob Marley"],
+            "answer": "Prince"
+        },
+        {
+            "question": "Which artist is known for the song 'Like a Rolling Stone'?",
+            "options": ["Bob Dylan", "Jimi Hendrix", "Eric Clapton", "Bruce Springsteen"],
+            "answer": "Bob Dylan"
+        },
+        {
+            "question": "Who is the lead vocalist of the band Coldplay?",
+            "options": ["Chris Martin", "Bono", "Thom Yorke", "Brandon Flowers"],
+            "answer": "Chris Martin"
+        },
+        {
+            "question": "Which artist released the album 'Born in the USA'?",
+            "options": ["Bruce Springsteen", "Tom Petty", "Neil Young", "John Mellencamp"],
+            "answer": "Bruce Springsteen"
+        },
+        {
+            "question": "Who is known for the song 'Purple Haze'?",
+            "options": ["Jimi Hendrix", "Jimmy Page", "Stevie Ray Vaughan", "Carlos Santana"],
+            "answer": "Jimi Hendrix"
+        },
+        {
+            "question": "Which artist is known for the album 'Abbey Road'?",
+            "options": ["The Beatles", "The Rolling Stones", "The Beach Boys", "The Who"],
+            "answer": "The Beatles"
+        },
+        {
+            "question": "Who is the lead vocalist of the band Queen?",
+            "options": ["Freddie Mercury", "Axl Rose", "Robert Plant", "Steven Tyler"],
+            "answer": "Freddie Mercury"
+        }
+    ],
+    "TV Shows": [
+        {
+            "question": "Which TV show is set in the fictional town of Springfield?",
+            "options": ["The Simpsons", "Friends", "Breaking Bad", "Game of Thrones"],
+            "answer": "The Simpsons"
+        },
+        {
+            "question": "Which TV show is set in the realm of Westeros?",
+            "options": ["Game of Thrones", "The Walking Dead", "Stranger Things", "The Big Bang Theory"],
+            "answer": "Game of Thrones"
+        },
+        {
+            "question": "Who is the protagonist in the TV show 'Breaking Bad'?",
+            "options": ["Walter White", "Don Draper", "Tony Soprano", "Dexter Morgan"],
+            "answer": "Walter White"
+        },
+        {
+            "question": "Which TV show is about a group of kids in the 1980s encountering supernatural forces?",
+            "options": ["Stranger Things", "The X-Files", "Lost", "Twin Peaks"],
+            "answer": "Stranger Things"
+        },
+        {
+            "question": "Which TV show is set in a paper supply company in Scranton, Pennsylvania?",
+            "options": ["The Office", "Parks and Recreation", "Brooklyn Nine-Nine", "The IT Crowd"],
+            "answer": "The Office"
+        },
+        {
+            "question": "Who is the main character in the TV show 'Lost'?",
+            "options": ["Jack Shephard", "Walter White", "Tony Soprano", "Dexter Morgan"],
+            "answer": "Jack Shephard"
+        },
+        {
+            "question": "Which TV show is about a group of friends working in a parks department in Pawnee, Indiana?",
+            "options": ["Parks and Recreation", "The Good Place", "Brooklyn Nine-Nine", "The Big Bang Theory"],
+            "answer": "Parks and Recreation"
+        }
+    ],
+    "Animal Planet": [
+        {
+            "question": "What is the largest species of shark?",
+            "options": ["Whale Shark", "Great White Shark", "Tiger Shark", "Hammerhead Shark"],
+            "answer": "Whale Shark"
+        },
+        {
+            "question": "What is the world's largest species of bear?",
+            "options": ["Polar Bear", "Grizzly Bear", "Brown Bear", "Black Bear"],
+            "answer": "Polar Bear"
+        },
+        {
+            "question": "Which animal has the largest brain relative to its body size?",
+            "options": ["Dolphin", "Elephant", "Human", "Octopus"],
+            "answer": "Octopus"
+        },
+        {
+            "question": "What is the only bird that can fly backward?",
+            "options": ["Hummingbird", "Eagle", "Ostrich", "Penguin"],
+            "answer": "Hummingbird"
+        },
+        {
+            "question": "What is the smallest species of monkey in the world?",
+            "options": ["Pygmy Marmoset", "Squirrel Monkey", "Tamarin", "Gibbon"],
+            "answer": "Pygmy Marmoset"
+        },
+        {
+            "question": "What is the largest species of venomous snake?",
+            "options": ["King Cobra", "Black Mamba", "Rattlesnake", "Inland Taipan"],
+            "answer": "King Cobra"
+        },
+        {
+            "question": "Which animal has the largest wingspan?",
+            "options": ["Wandering Albatross", "Bald Eagle", "Golden Eagle", "Peregrine Falcon"],
+            "answer": "Wandering Albatross"
+        },
+        {
+            "question": "Which species of turtle is known for its long lifespan?",
+            "options": ["Galapagos Tortoise", "Leatherback Turtle", "Snapping Turtle", "Box Turtle"],
+            "answer": "Galapagos Tortoise"
+        },
+        {
+            "question": "Which animal has the longest gestation period?",
+            "options": ["Elephant", "Giraffe", "Whale", "Horse"],
+            "answer": "Elephant"
+        },
+        {
+            "question": "What is the fastest swimming fish in the ocean?",
+            "options": ["Sailfish", "Tuna", "Marlin", "Barracuda"],
+            "answer": "Sailfish"
+        },
+        {
+            "question": "Which animal is known for its ability to change its sex?",
+            "options": ["Clownfish", "Seahorse", "Anglerfish", "Parrotfish"],
+            "answer": "Parrotfish"
+        }
+    ]
+}
