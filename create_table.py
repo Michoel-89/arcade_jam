4555912e3ceb9bb3a94b557747366983e388e22b
michoel <michoelr111@gmail.com>
2023-06-27 11:36:17 -0400

    deleted game.db and started working on sqlite

--- create_table.py
+++ create_table.py
@@ -1,4 +1,10 @@
 import sqlite3
-from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, Integer, String, create_engine
+
+Base = declarative_base()
+
+class Game(Base):
+    engine = create_engine('sqlite:///game')
 
 
